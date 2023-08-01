2e75f64b814efe34b36f234b1789a3a62c26f402
Noel Lim <noellimx@gmail.com>
2023-08-01 16:35:34 +0800

    allow /auth to be unauthenticated

--- src/main/java/com/noellimx/main/configuration/SecurityConfiguration.java
+++ src/main/java/com/noellimx/main/configuration/SecurityConfiguration.java
@@ -1,11 +1,7 @@
 package com.noellimx.main.configuration;
 
-import com.noellimx.main.service.MyUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,27 +16,16 @@ public class SecurityConfiguration {
     return new BCryptPasswordEncoder();
   }
 
-  @Bean
-  @Autowired
-  public AuthenticationManager authenticationManager(HttpSecurity http,
-      MyUserDetailsService userDetailsService,
-      PasswordEncoder bCryptPasswordEncoder)
-      throws Exception {
-    AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
-        AuthenticationManagerBuilder.class);
-    authenticationManagerBuilder.userDetailsService(userDetailsService)
-        .passwordEncoder(bCryptPasswordEncoder);
-
-    return authenticationManagerBuilder.build();
-  }
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable());
 
-//    http.authorizeHttpRequests((authorizeHttpRequests) ->
-//        authorizeHttpRequests.anyRequest().authenticated()
-//    );
+    http.authorizeHttpRequests((authorizeHttpRequests) ->
+        authorizeHttpRequests
+            .requestMatchers("/auth").permitAll()
+            .anyRequest().permitAll()
+    );
 
     http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
