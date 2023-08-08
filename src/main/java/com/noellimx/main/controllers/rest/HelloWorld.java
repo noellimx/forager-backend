75dce780f8f55f2b16654e359e45fa5f7afc0f48
Noel Lim <noellimx@gmail.com>
2023-08-09 03:10:50 +0800

    - change hello string to bagrocketglidebus;

--- src/main/java/com/noellimx/main/controllers/rest/HelloWorld.java
+++ src/main/java/com/noellimx/main/controllers/rest/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
 
   @GetMapping("/")
   public String sayHello() {
-    return "Hello at / " + venus.some() + "1691510686";
+    return "Hello at / " + venus.some() + "bagrocketglidebus";
   }
 
   @GetMapping("/world")
